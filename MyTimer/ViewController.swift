4905ea145b525604e4f1f9532c5fea968fffd2ec
nahotomiki <naho_tech@icloud.com>
2021-12-31 20:37:39 +0900

    Initial Commit

---
+++ MyTimer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTimer
+//
+//  Created by 富木菜穂 on 2021/12/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
